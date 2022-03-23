3a458ca2060dce82a1830901fc8ced9fc88e06f4
Niiivek <kevinchen.pro@gmail.com>
2022-03-23 16:06:30 +0100

    matrice

--- fichier_list.py
+++ fichier_list.py
@@ -1,6 +1,6 @@
 import csv
 import time
-
+from graphe_pagerank import pagerank_sparse
 def conversion_list_fichier_graphe(fichier):
     ligne =[]
     colonne = []
@@ -18,3 +18,6 @@ t2 = time.time()
 print(t2-t1)
 for i in range(5):
     print(ligne[i],colonne[i])
+
+page_rank=pagerank_sparse(ligne,colonne,0.85,0.000001)
+print(page_rank)
