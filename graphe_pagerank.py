b4689761d0703dec4de018409fcb1943cd701be0
Niiivek <kevinchen.pro@gmail.com>
2022-04-06 15:17:25 +0200

    fonction a jour

--- graphe_pagerank.py
+++ graphe_pagerank.py
@@ -126,6 +126,29 @@ def forward_push(dico, alpha, epsilon):
 		counter += 1
 	return p
 
+#Forward push avec dictionnaire en entrée page rank perso
+def forward_push_perso(dico, alpha, epsilon,liste):
+	size = len(dico)
+	#creation y
+	y=np.zeros(size)
+	for i in liste:
+		y[i]=1
+	r = y*(1-alpha)/len(liste) # ici r c'est y*(1-alpha)
+	p = np.zeros(size)
+	counter = 0
+	while max( r ) > epsilon : 
+		u = np.argmax( r )
+		x = r[u]
+		p[u] += x
+		r[u] -= x
+		
+		voisins = dico[u]
+		num_voisins = len(voisins)
+		for i in voisins:
+			r[i] += alpha*x/num_voisins		
+		counter += 1
+	return p
+
 #Fonction qui calcule la page rank perso avec somme
 def power_iteration_somme_perso(dict1,alpha,epsilon,liste):
     nb_sommet=len(dict1)
