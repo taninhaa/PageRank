69d030dc41eaa83f3fb3e8777dd536281725fa0f
Niiivek <kevinchen.pro@gmail.com>
2022-04-06 16:09:49 +0200

    tout

--- graphe_pagerank.py
+++ graphe_pagerank.py
@@ -2,10 +2,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 import wikipediaapi
 import scipy.sparse as sps
+import scipy.sparse.linalg as spl
 import seaborn
 import csv
 
-def creation_p(dict2): # conversion de notre dictionnaire à la matrice de transition
+#Fonction qui va lire un fichier et qui donne deux tableaux avec les lignes et les colonnes
+def reader_lists(fileName):
+	with open(fileName, 'r') as csvfile:
+		rows = []
+		cols = []
+		edgelist = csv.reader(csvfile, delimiter=' ')
+		for line in edgelist:
+			rows.append( int(line[0]) )
+			cols.append( int(line[1]) )
+		return rows, cols
+
+#Fonction qui va convertir les deux tableaux lignes , colonnes et qui donne le dictionnaire
+def dico_construction(rows,cols):
+	size = len(rows)
+	dico = dict()
+	for i in range(size):
+		if rows[i] not in dico: dico[rows[i]] = set([rows[i]])	
+		if cols[i] not in dico: dico[cols[i]] = set([cols[i]])
+		dico[rows[i]].add( cols[i] )
+	return dico
+
+#Fonction qui convertit un dictionnaire en matrice de transition
+def creation_p(dict2): 
     nb_sommet=len(dict2)
     P=np.zeros((nb_sommet,nb_sommet))
     for cle in dict2:
@@ -13,7 +36,26 @@ def creation_p(dict2): # conversion de notre dictionnaire à la matrice de trans
             P[cle,sommet]=1/len(dict2[cle])
     return P
 
-def power_iteration_matrice_erreur(dict2,alpha,epsilon): # calcul de la valeur du page rank de manière matricielle avec erreur epsilon
+#Fonction qui calcule la page rank avec la formule de la somme
+def power_iteration_somme(dict1,alpha,epsilon):
+    nb_sommet=len(dict1)
+    constante=(1-alpha)/nb_sommet
+    x0=np.zeros(nb_sommet)
+    x1=np.ones(nb_sommet)
+    iteration=0
+    while(np.linalg.norm(x1-x0)>epsilon): 
+        iteration=iteration+1
+        x0=np.copy(x1)
+        for cles in dict1: 
+            somme=0
+            for cles2 in dict1: 
+                if cles in dict1[cles2]:
+                    somme=somme+(x0[cles2]/len(dict1[cles2]))
+            x1[cles]=alpha*somme+constante
+    return x1,iteration
+
+#Fonction qui calcule la valeur du page rank de manière matricielle en prenant en argument un dictionnaire
+def power_iteration(dict2,alpha,epsilon):
     nb_sommet=len(dict2)
     P=creation_p(dict2)
     e=np.array([(1-alpha)/nb_sommet]*nb_sommet)
@@ -24,28 +66,8 @@ def power_iteration_matrice_erreur(dict2,alpha,epsilon): # calcul de la valeur d
         pi=alpha*np.dot(pi_avant,P)+e
     return pi
 
-def reader_lists(fileName):
-	with open(fileName, 'r') as csvfile:
-		rows = []
-		cols = []
-		edgelist = csv.reader(csvfile, delimiter=' ')
-		for line in edgelist:
-			rows.append( int(line[0]) )
-			cols.append( int(line[1]) )
-
-		return rows, cols
-
-def dict_sparse2(dict2): # retourne les indices de lignes et de colonnes des éléments non nuls
-    data=[]
-    row_ind=[]
-    col_ind=[]
-    for cle in dict2:
-        for sommet in dict2[cle]:
-            row_ind.append(cle)
-            col_ind.append(sommet)
-    return row_ind,col_ind
-
-def pagerank_sparse(rows, cols, alpha, epsilon): #calcul du page rank sans utiliser le dictionnaire
+#Fonction qui calcule la valeur du page rank sans utiliser le dictionnaire avec sparse
+def pagerank_sparse(rows, cols, alpha, epsilon): 
 	size = max(max(rows), max(cols)) + 1
 	A = sps.csr_matrix(([1]*len(rows), (rows, cols)), shape=(size,size))
 	A = A + sps.eye(size)
@@ -62,3 +84,114 @@ def pagerank_sparse(rows, cols, alpha, epsilon): #calcul du page rank sans utili
 		pi_avant=pi
 		pi=alpha*(Pt.dot(pi_avant))+e
 	return pi
+
+#Fonction qui calcule la valeur exacte du page rank en prenant en entrée tableau de lignes et de colonnes 
+def exact_pr(rows, cols, alpha):
+	size = max(max(rows), max(cols)) + 1
+	A = sps.csr_matrix(([1]*len(rows), (rows, cols)), shape=(size,size))
+	A = A + sps.eye(size)
+	D = A.dot([1]*size)
+	Dinv = sps.diags([np.reciprocal(D)], [0])
+	P = Dinv.dot( A ).transpose()
+	e = [1/size]*size
+	M = sps.eye(size) - alpha*P
+	pr_exact = spl.spsolve(M, e)
+	return pr_exact
+
+def dico_construction(rows,cols):
+	size = len(rows)
+	dico = dict()
+	for i in range(size):
+		if rows[i] not in dico: dico[rows[i]] = set([rows[i]])	
+		if cols[i] not in dico: dico[cols[i]] = set([cols[i]])
+		dico[rows[i]].add( cols[i] )
+	return dico
+	
+#Forward push avec dictionnaire en entrée
+def forward_push(dico, alpha, epsilon):
+	size = len(dico)
+	r = np.array([(1-alpha)/size]*size)
+	p = np.zeros(size)
+	counter = 0
+	while max( r ) > epsilon : 
+		u = np.argmax( r )
+		x = r[u]
+		p[u] += x
+		r[u] -= x
+		
+		voisins = dico[u]
+		num_voisins = len(voisins)
+		for i in voisins:
+			r[i] += alpha*x/num_voisins		
+		counter += 1
+	return p
+
+#Forward push avec dictionnaire en entrée page rank perso
+def forward_push_perso(dico, alpha, epsilon,liste):
+	size = len(dico)
+	#creation y
+	y=np.zeros(size)
+	for i in liste:
+		y[i]=1
+	r = y*(1-alpha)/len(liste) # ici r c'est y*(1-alpha)
+	p = np.zeros(size)
+	counter = 0
+	while max( r ) > epsilon : 
+		u = np.argmax( r )
+		x = r[u]
+		p[u] += x
+		r[u] -= x
+		
+		voisins = dico[u]
+		num_voisins = len(voisins)
+		for i in voisins:
+			r[i] += alpha*x/num_voisins		
+		counter += 1
+	return p
+
+#Fonction qui calcule la page rank perso avec somme
+def power_iteration_somme_perso(dict1,alpha,epsilon,liste):
+    nb_sommet=len(dict1)
+
+    #creation y
+    y=np.zeros(nb_sommet)
+    for i in liste:
+        y[i]=1
+    x1=y/len(liste)
+    y=(1-alpha)*y/len(liste)
+    x0=np.zeros(nb_sommet)
+    iteration=0
+    while(np.linalg.norm(x1-x0)>epsilon): 
+        iteration=iteration+1
+        x0=np.copy(x1)
+        for cles in dict1: 
+            somme=0
+            for cles2 in dict1: 
+                if cles in dict1[cles2]:
+                    somme=somme+(x0[cles2]/len(dict1[cles2]))
+            x1[cles]=(alpha*somme)+y[cles]
+    return x1,iteration
+
+#Fonction qui calcule la page rank perso de manière matricielle
+def pagerank_sparse_perso(rows, cols, alpha, epsilon,liste):
+	size = max(max(rows), max(cols)) + 1
+	A = sps.csr_matrix(([1]*len(rows), (rows, cols)), shape=(size,size))
+	A = A + sps.eye(size)
+	D = A.dot([1]*size)
+	Dinv = sps.diags([np.reciprocal(D)], [0])
+	P = Dinv.dot(A)
+
+	#creation de y
+	y=np.zeros(size)
+	for i in liste:
+		y[i]=1
+	pi=np.array(y)
+	y=(1-alpha)*y/len(liste)
+	pi_avant=[0]*size
+	Pt=P.transpose()
+	iteration=0
+	while(np.linalg.norm(pi-pi_avant)>epsilon):
+		pi_avant=pi
+		pi=alpha*(Pt.dot(pi_avant))+y
+		iteration=iteration+1
+	return pi,iteration
