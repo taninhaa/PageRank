420462094624f56e22ef8d840bed2fd6d4290982
BastienG27 <bastien.guillemare@gmail.com>
2022-04-08 19:09:07 +0200

    Script bash fontionnel: Renvoie les 5meilleurs

--- wiki_search.sh
+++ wiki_search.sh
@@ -12,20 +12,26 @@ ChercheID(){
     do
         IDPagerank+=($(grep "^$id\\b" pagerank.txt))
     done
-    
 }
 
 #Si le titre $1 n'est pas vide on cherche les ID
 if test -n "$1" 
 then
     ChercheID $1
+    BestId=$(./triPagerank ${IDPagerank[*]}|awk -F'\t' 'NR < 6 {print $1}')
+    
+    i=1
+    for id in $BestId
+    do
+        echo -n "$i >> " 
+        awk -v a=$id -F'\t' 'BEGIN{a++} NR == a {print $2}' id-titre.txt
+        i=$((i+1))
+    done
+    
 fi
 
 #print test
-for val in ${IDPagerank[@]} 
-do  
-    echo $val
-done
+
 
 #awk -v a=$1 'BEGIN{var=a} ($2 ~ (a)) {print $1,$2}' id-titre.txt
 
