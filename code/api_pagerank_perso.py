84a065d0ef063e9c318dfa2229d66c69c716cdfa
BastienG27 <bastien.guillemare@gmail.com>
2022-05-08 18:55:31 +0200

    Ajout de l'option -p|-perso qui calcule le pagerank perso via l'api bash wiki_search.sh

--- code/api_pagerank_perso.py
+++ code/api_pagerank_perso.py
@@ -1,14 +1,27 @@
-from operator import imod
 import sys
 from graphe_pagerank import reader_lists,pagerank_sparse_perso
 
+#On recupère les ID
 liste_ID = []
-for ID in sys.argv:
-    liste_ID.append(ID)
+for ID in sys.argv[1:]: #sys.argv[0] == './code/api_pagerank_perso.py'
+    liste_ID.append(int(ID))
 
-rows,cols = reader_lists("../data/aretes.txt")
-alpha = 1
-eps = 10**-6
-
+#On recupère l'ensemble des arêtes sous forme matricielle
+rows,cols = reader_lists("./data/aretes.txt")
+alpha = 0.85
+eps = 1e-10
+#Calcule du pagerank personnalisé qui prend en compte les ID personnalisés
 pi,iter = pagerank_sparse_perso(rows,cols,alpha,eps,liste_ID)
-print(pi)
+
+
+n_res = 0
+#On renvoie les 5 meilleurs ID dans le script 
+for ID in (-pi).argsort():
+    if n_res == 5:
+        break
+    elif pi[ID] != 0:
+        #On s'assure que le pagerank n'est pas nul
+        n_res += 1
+        print(ID)
+        
+
